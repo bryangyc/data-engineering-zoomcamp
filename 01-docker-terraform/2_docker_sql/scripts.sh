1d1560db9ce3319bff596017b6428ee27dd8a774
bryangyc <bryan.gohyc@gmail.com>
2024-08-26 00:56:24 +0800

    feat: script to call

---
+++ 01-docker-terraform/2_docker_sql/scripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+python ingest_data.py \
+  --user=root \
+  --password=root \
+  --host=localhost \
+  --port=5432 \
+  --db=ny_taxi \
+  --table_name=yellow_taxi_trips \
+  --url=${URL}
+
+URL="https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
+
+python ingest_data.py \
+  --user=root \
+  --password=root \
+  --host=localhost \
+  --port=5432 \
+  --db=ny_taxi \
+  --table_name=zones \
+  --url=${URL}
